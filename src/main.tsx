da0c5dd87ccaa664b738364c66dd1694e33ce153
Luca8991 <luca.berte98@hotmail.it>
2023-07-19 19:21:53 +0200

    fix: always dark color mode

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App.tsx'
+import theme from './theme.ts'
 import './index.css'
 
 console.log('Version:', import.meta.env.VITE_COMMIT_HASH)
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <App />
     </ChakraProvider>
   </React.StrictMode>,
